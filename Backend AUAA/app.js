2b7fd1372ebe6111255f53647c79b47c9cbde0c8
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-02-02 20:49:53 +0530

    user-controller.js constructed

--- Backend AUAA/app.js
+++ Backend AUAA/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import router from './routes/user-routes'
 dotenv.config()
 const app = express();
+app.use('/api',router)
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
     app.listen(5000);
