b30f6d89c1bd2463e1e313e2b853abc618d04d9c
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-02-03 11:09:05 +0530

    Duplicate Email SignUp -Solved

--- Backend AUAA/controllers/user-controller.js
+++ Backend AUAA/controllers/user-controller.js
@@ -1,11 +1,19 @@
 import User from '../model/User.js'
 const signup  = async(req,res,next)=>{
-    const user = new User({
-        name : req.body.name,
-        email : req.body.email,
-        password : req.body.password
-    });
+    const {name,email,password} = req.body;
+    let existingUser;
     try{
+        existingUser = await User.findOne({email:email})
+    }catch(err){console.log(err)}
+    if(existingUser){
+        return res.status(400).json({message : "user already exists! Login Instead"})
+    }
+    const user = new User({
+        name,
+        email,
+        password
+    });
+    try{ 
         await user.save();
     }catch(err){console.log(err)}
     return res.status(201).json({message : user})
