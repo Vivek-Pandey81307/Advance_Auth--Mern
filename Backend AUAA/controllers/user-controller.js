38c4b8c374dd2844de8eb6c85b0c2d98caa24053
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-02-06 21:36:06 +0530

    VerifyToken

--- Backend AUAA/controllers/user-controller.js
+++ Backend AUAA/controllers/user-controller.js
@@ -1,70 +1,51 @@
-import User from "../model/User.js";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
-const signup = async (req, res, next) => {
-  const { name, email, password } = req.body;
-  let existingUser;
-  try {
-    existingUser = await User.findOne({ email: email });
-  } catch (err) {
-    console.log(err);
-  }
-  if (existingUser) {
-    return res
-      .status(400)
-      .json({ message: "user already exists! Login Instead" });
-  }
-  const hashedPassword = bcrypt.hashSync(password);
-  const user = new User({
-    name,
-    email,
-    password: hashedPassword,
-  });
-  try {
-    await user.save();
-  } catch (err) {
-    console.log(err);
-  }
-  return res.status(201).json({ message: user });
-};
-const login = async (req, res, next) => {
-  let inputEmail;
-  try {
-    inputEmail = await User.findOne({ email: req.body.email });
-  } catch (err) {
-    console.log(err);
-    return res
-      .status(404)
-      .json({ message: "User with provided email doesn't exist" });
-  }
-  const hashedInputPassword = bcrypt.hashSync(req.body.password);
-  if (!bcrypt.compareSync(req.body.password, inputEmail.password)) {
-    return res.status(401).json({ message: "Incorrect Password" });
-  }
-  const token = jwt.sign({ id: inputEmail.__id }, process.env.SECRET_KEY, {
-    expiresIn: "4hr",
-  });
-  return res
-    .status(200)
-    .json({
-      message: "Successfully login with registered email!",
-      user: inputEmail,
-      token,
-    });
-};
-const verifytoken = (req, res, next) => {
-  const headers = req.headers.authorization;
-  const token = headers.split(" ")[1];
-  if (!token) {
-    return res.status(404).json({ message: "No token found" });
-  }
-  jwt.verify(String(token), process.env.SECRET_KEY, (err, user) => {
-    if (err) {
-      return res.status(404).json({ message: "Token doesn't match" });
+import User from '../model/User.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
+const signup  = async(req,res,next)=>{
+    const {name,email,password} = req.body;
+    let existingUser;
+    try{
+        existingUser = await User.findOne({email:email})
+    }catch(err){console.log(err)}
+    if(existingUser){
+        return res.status(400).json({message : "user already exists! Login Instead"})
     }
-    console.log(user.id);
-  });
-};
-export { signup, login, verifytoken };
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = new User({
+        name,
+        email,
+        password : hashedPassword
+    });
+    try{ 
+        await user.save();
+    }catch(err){console.log(err)}
+    return res.status(201).json({message : user})
+    
+}
+const login = async(req,res,next)=>{
+    let inputEmail
+    try{
+        inputEmail = await User.findOne({email:req.body.email})
+    }catch(err){console.log(err);return res.status(404).json({message : "User with provided email doesn't exist"})}
+    const hashedInputPassword = bcrypt.hashSync(req.body.password)
+    if(!bcrypt.compareSync(req.body.password,inputEmail.password)){
+       return res.status(401).json({message : "Incorrect Password"})
+    }
+    const token = jwt.sign({id:inputEmail._id},process.env.SECRET_KEY,{expiresIn : "4hr"})
+    return res.status(200).json({message:"Successfully login with registered email!",user:inputEmail,token})
+ 
+}
+const verifyToken  = (req,res,next)=>{
+    const headers = req.headers.authorization;
+    const token = headers.split(" ")[1];
+    if(!token){return res.status(404).json({message : "No token found"})}
+    jwt.verify(String(token),process.env.SECRET_KEY,(err,user)=>{
+        if(err){
+           return  res.status(404).json({message:"Token doesn't match"})
+        }console.log(user.id);
+        
+    })
+}
+export {signup,login,verifyToken};
--- Backend AUAA/routes/user-routes.js
+++ Backend AUAA/routes/user-routes.js
@@ -1,7 +1,7 @@
 import express from 'express'
-import { signup,login, verifytoken } from '../controllers/user-controller.js';
+import { signup,login, verifyToken } from '../controllers/user-controller.js';
 const router = express.Router();
 router.post("/signup",signup);
 router.post("/login",login);
-router.get("/user",verifytoken)
+router.get("/user",verifyToken)
 export default router
