b11a308070d2516d0c5b0a17bd23033f5c39f8f0
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-02-07 23:59:40 +0530

    header

---
+++ Frontend AUAA/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {AppBar, Typography} from '@mui/material'
+import App from '../App'
+const Header = () => {
+  return (
+    <div><AppBar><Toolbar><Typography variant='h2'>Hello World!</Typography></Toolbar></AppBar></div>
+  )
+}
+
+export default Header
