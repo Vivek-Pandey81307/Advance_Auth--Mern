3051e5b705617e6d5da7a7f7e36d111d3a7e7cb0
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-02-08 21:34:21 +0530

    Assigning Navlinks to Tabs

--- Frontend AUAA/src/App.js
+++ Frontend AUAA/src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
+import React from 'react';
+import {Route,Routes} from 'react-router-dom'
 import Header from './components/Header';
+import Login from './components/Login'
+import Signup from './components/Signup';
+import Welcome from './components/Welcome';
 function App() {
   return (
     <React.Fragment>
@@ -9,7 +14,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/login" element={<Login/>} />
-          <Route path="/Signup"element={<Signup/>}/>
+          <Route path="/signup" element={<Signup/>}/>
           <Route path="/user" element={<Welcome/>} />
         </Routes>
       </main>
--- Frontend AUAA/src/components/Header.js
+++ Frontend AUAA/src/components/Header.js
@@ -1,17 +1,17 @@
 import React,{useState} from "react";
-import { AppBar,Box, Typography, Toolbar, Tabs,Tab } from "@mui/material";
-import App from "../App";
+import { Link,AppBar,Box, Typography, Toolbar, Tabs,Tab } from "@mui/material";
+import { NavLink } from "react-router-dom";
 const Header = () => {
-    const [value,setValue] =useState()
+    const [value,setValue] =useState(0)
   return (
     <div>
-      <AppBar>
+      <AppBar position="sticky">
         <Toolbar>
           <Typography variant="h2">MernAuth</Typography>
           <Box sx={{marginLeft:"auto"}}>
-        <Tabs indicatorColor="secondary" value={value} onChange={(e,val)=>{setValue(val);console.log(val)}} textColor = "inherit">
-          <Tab label="Login" />
-          <Tab label="Signup"/>
+        <Tabs indicatorColor="secondary" value={value} onChange={(e,val)=>setValue(val)} textColor = "inherit">
+          <Tab to="/login" LinkComponent={NavLink} label="Login" />
+          <Tab to="/signup" LinkComponent={NavLink} label="Signup"/>
         </Tabs>
       </Box>
         </Toolbar>
